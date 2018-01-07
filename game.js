ad800b1516c3ed45a67750a30aa62321033dfff2
Goodscout <marta.lewsza@gmail.com>
2018-01-07 16:37:07 +0100

    ready game

--- game.js
+++ game.js
@@ -1,79 +1,125 @@
 //zapobiegamy że divy się jeszcze nie utworza zanim JS zacznie je wyszukiwac, ten listner bedzie dbal  to aby caly dokumement sie zaladowal
 
 document.addEventListener('DOMContentLoaded', function() {
-    var fields = document.querySelectorAll('.board > div'); //wszystko co ma klasę board i jest divem jest zaznaczone
-    var emptyFields;
-
-    //deklarujemy obiekt 
+	var resetButton = document.getElementById('reset-score');
     var playerClasses = {
-        'playerA': 'red',
+        'playerA': 'red', 
         'playerB': 'blue'
     };
+
+    var scores = {
+		'playerA': 0,
+		'playerB': 0
+	}
+
     var currentPlayer;
+	var emptyFields;
 
     initGame();
 
+    resetButton.addEventListener('click', function() {
+    	scores['playerA'] = 0;
+    	scores['playerB'] = 0;
+
+		displayPlayerScore('playerA');
+		displayPlayerScore('playerB');
+	});
+
+	function displayPlayerScore(player) {
+		var score = document.getElementById(`${player}-score`);
+
+		score.innerHTML = `${player} score: ${scores[player]}`;
+	}
+
+	function updatePlayerScore(player) {
+		scores[player]++;
+	}
+
+    function displayRoundInformation() {
+		var round = document.getElementById('round-info');
+
+		round.className = playerClasses[currentPlayer];
+		round.innerHTML = `Round for ${currentPlayer}`;
+    }
+
     function initGame() {
-        emptyFields = 9;
-        console.log('dziala');
+		var fields = document.querySelectorAll('.board > div');
 
-        //ustawiamy playera A jako inicjującego
+		currentPlayer = 'playerA';
+		emptyFields = 9;
+    	fields.forEach(field => field.addEventListener('click', fieldClickHandler));
+		fields.forEach(field => field.removeAttribute('class'));
 
-        currentPlayer = 'playerA';
+		displayRoundInformation();
+		displayPlayerScore('playerA');
+		displayPlayerScore('playerB');
+	}
 
-    fields.forEach(field => {
-        field.addEventListener('click', fieldClickHandler)//iterujesz po każdym elemencie field i jak jest kliknęcie to wywołuje się funkcja fieldClickHandler
-        field.className ='';    
-    });    
-    }
+	function fieldClickHandler () {
+        var playerClass = playerClasses[currentPlayer];
+        this.classList.add(playerClass);
 
-    function fieldClickHandler() {
-            var playerClass = playerClasses[currentPlayer];
-            console.log('clicked', this);//wywołana pokażdym kliknięciu, w konsoli pojawia się napis clicked przy każdym divie
-            this.classList.add(playerClass);
-            
-//jeśli playerA klika to potem się ma zmieniac na playeraB 
-        if(currentPlayer === 'playerA') {
-            currentPlayer = 'playerB';
-        } 
-        else {
-            currentPlayer = 'playerA';
-        }
-        this.removeEventListener('click', fieldClickHandler);
-        emptyFields--; //odejmuje o 1, mozesz zapisać "emptyFields = emptyFields - 1"albo emptyFields -=1
-       checkWinner();
-        if (emptyFields === 0) {
-            setTimeout(function () {
-                alert('Game over')
+		emptyFields--;
+        currentPlayer = currentPlayer === 'playerA' ? 'playerB' : 'playerA';
+
+		displayRoundInformation();
+		this.removeEventListener('click', fieldClickHandler);
+
+		checkWinner();
+	}
+
+	function  checkWinner() {
+		var fields = document.querySelectorAll('.board > div');
+
+		// Horizontal winning configurations
+		var row1 = fields[0].className + fields[1].className + fields[2].className;
+		var row2 = fields[3].className + fields[4].className + fields[5].className;
+		var row3 = fields[6].className + fields[7].className + fields[8].className;
+
+		// Vertical winning configurations
+		var column1 = fields[0].className + fields[3].className + fields[6].className;
+		var column2 = fields[1].className + fields[4].className + fields[7].className;
+		var column3 = fields[2].className + fields[5].className + fields[8].className;
+
+		// Diagonal winning configurations
+		var diagonal1 = fields[0].className + fields[4].className + fields[8].className;
+		var diagonal2 = fields[6].className + fields[4].className + fields[2].className;
+
+		var boardCheck = [
+			row1,
+			row2,
+			row3,
+			column1,
+			column2,
+			column3,
+			diagonal1,
+			diagonal2
+		];
+
+		if (boardCheck.includes('redredred')) {
+			setTimeout(() => {
+				alert("Player A - YOU ROCK !!!");
+				updatePlayerScore('playerA');
+				initGame();
+			}, 100);
+			return;
+		}
+
+		if (boardCheck.includes('blueblueblue')) {
+			setTimeout(() => {
+				alert("Player B - YOU ROCK !!!");
+				updatePlayerScore('playerB');
+				initGame();
+			}, 100);
+			return;
+		}
+
+		if(emptyFields === 0) {
+            setTimeout(() => {
+                alert('TRY AGAIN');
                 initGame();
-            }, 100)
-       }
-    }
-    function checkWinner() {
-        var row1 = fields[0].className + fields[1].className + fields[2].className;
-        var row2 = fields[3].className + fields[4].className + fields[5].className;
-        var row3 = fields[6].className + fields[7].className + fields[8].className;
-        
-        var col1 = fields[0].className + fields[3].className + fields[6].className;
-        var col2 = fields[1].className + fields[4].className + fields[7].className
-        var col3 = fields[2].className + fields[5].className + fields[8].className;
-
-        var diag1 = fields[0].className + fields[4].className + fields[8].className;
-        var diag2 = fields[2].className + fields[4].className + fields[6].className;
-
-        if (row1 === 'redredred'|| row2 === 'redredred' || row3 === 'redredred' || col1 === 'redredred' || col2 === 'redredred' || col3 === 'redredred' || diag1 === 'redredred' || diag2 === 'redredred') {
-
-            setTimeout(function () {
-                alert('playerA won!')
-                initGame();
-            }, 100) 
-        }
-        if (row1 === 'blueblueblue'|| row2 === 'blueblueblue' || row3 === 'blueblueblue' || col1 === 'blueblueblue' || col2 === 'blueblueblue' || col3 === 'blueblueblue' || diag1 === 'blueblueblue' || diag2 === 'blueblueblue') {
-            
-            setTimeout(function () {
-                alert('playerB won!')
-                initGame();
-            }, 100) 
-        }
-    }
+            }, 100);
+            return;
+		}
+	}
 });
