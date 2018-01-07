b765268e008343bba337f55cf28e8ff376443a87
Goodscout <marta.lewsza@gmail.com>
2018-01-07 11:24:58 +0100

    step2

--- game.js
+++ game.js
@@ -5,14 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
     initGame();
 
     function initGame() {
-        var fields = document.querySelectorAll('.board > div');
+        var fields = document.querySelectorAll('.board > div');//wszystko co ma klasę board i jest divem jest zaznaczone
 
     fields.forEach(field => {
-        field.addEventListener('click', fieldClickHandler)
+        field.addEventListener('click', fieldClickHandler)//iterujesz po każdym elemencie field i jak jest kliknęcie to wywołuje się funkcja fieldClickHandler
         });
     }
 
     function fieldClickHandler() {
-            console.log('clicked', this);
+            console.log('clicked', this);//wywołana pokażdym kliknięciu, w konsoli pojawia się napis clicked przy każdym divie
         }
 });
