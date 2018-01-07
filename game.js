0eac1398859136759c8cb48d8537075a564423f8
Goodscout <marta.lewsza@gmail.com>
2018-01-07 12:23:50 +0100

    step 3

--- game.js
+++ game.js
@@ -2,10 +2,21 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
+    //deklarujemy obiekt 
+    var playerClasses = {
+        'playerA': 'red',
+        'playerB': 'blue'
+    };
+    var currentPlayer;
+
     initGame();
 
     function initGame() {
         var fields = document.querySelectorAll('.board > div');//wszystko co ma klasę board i jest divem jest zaznaczone
+
+        //ustawiamy playera A jako inicjującego
+
+        currentPlayer = 'playerA';
 
     fields.forEach(field => {
         field.addEventListener('click', fieldClickHandler)//iterujesz po każdym elemencie field i jak jest kliknęcie to wywołuje się funkcja fieldClickHandler
@@ -13,7 +24,15 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function fieldClickHandler() {
+            var playerClass = playerClasses[currentPlayer];
             console.log('clicked', this);//wywołana pokażdym kliknięciu, w konsoli pojawia się napis clicked przy każdym divie
-            this.classList.add("red");
+            this.classList.add(playerClass);
+//jeśli playerA klika to potem się ma zmieniac na playeraB 
+        if(currentPlayer === 'playerA') {
+            currentPlayer = 'playerB';
+        } 
+        else {
+            currentPlayer = 'playerA';
         }
+     }
 });
