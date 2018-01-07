00638d36be75cbe899a7355e9f980c073d4fa47c
Goodscout <marta.lewsza@gmail.com>
2018-01-07 13:19:28 +0100

    step6

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 //zapobiegamy że divy się jeszcze nie utworza zanim JS zacznie je wyszukiwac, ten listner bedzie dbal  to aby caly dokumement sie zaladowal
 
 document.addEventListener('DOMContentLoaded', function() {
-
+    var fields = document.querySelectorAll('.board > div'); //wszystko co ma klasę board i jest divem jest zaznaczone
     var emptyFields;
 
     //deklarujemy obiekt 
@@ -15,8 +15,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function initGame() {
         emptyFields = 9;
-
-        var fields = document.querySelectorAll('.board > div');//wszystko co ma klasę board i jest divem jest zaznaczone
 
         //ustawiamy playera A jako inicjującego
 
@@ -41,8 +39,30 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         this.removeEventListener('click', fieldClickHandler);
         emptyFields--; //odejmuje o 1, mozesz zapisać "emptyFields = emptyFields - 1"albo emptyFields -=1
-       if (emptyFields === 0) {
-           alert('Co bys nie zrobił Marta i tak wygrała:)')
+       checkWinner();
+        if (emptyFields === 0) {
+           alert('game over')
        }
-     }
+    }
+    function checkWinner() {
+        var row1 = fields[0].className + fields[1].className + fields[2].className;
+        var row2 = fields[3].className + fields[4].className + fields[5].className;
+        var row3 = fields[6].className + fields[7].className + fields[8].className;
+        
+        var col1 = fields[0].className + fields[3].className + fields[6].className;
+        var col2 = fields[1].className + fields[4].className + fields[7].className
+        var col3 = fields[2].className + fields[5].className + fields[8].className;
+
+        var diag1 = fields[0].className + fields[4].className + fields[8].className;
+        var diag2 = fields[2].className + fields[4].className + fields[6].className;
+
+        if (row1 === 'redredred'|| row2 === 'redredred' || row3 === 'redredred' || col1 === 'redredred' || col2 === 'redredred' || col3 === 'redredred' || diag1 === 'redredred' || diag2 === 'redredred') {
+
+            alert('playerA won!')
+        }
+        if (row1 === 'blueblueblue'|| row2 === 'blueblueblue' || row3 === 'blueblueblue' || col1 === 'blueblueblue' || col2 === 'blueblueblue' || col3 === 'blueblueblue' || diag1 === 'blueblueblue' || diag2 === 'blueblueblue') {
+            
+            alert('playerB won!')
+        }
+    }   
 });
